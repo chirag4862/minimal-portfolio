d3c7bc7ba57ea95fd10f432baa5dbb948fa9ec60
chirag4862 <chiragrvijay4862@gmail.com>
2023-03-25 19:37:05 +0530

    Image Update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: "Movie DB",
-    imgUrl: "../src/assets/MovieDB.png",
+    imgUrl: "./public/assets/MovieDB.png",
     stack: ["SvelteKit", "HTML", "CSS", "JavaScript"],
     link: "https://movie-db-zeta-two.vercel.app/",
   },
   {
     title: "Hive Mind",
-    imgUrl: "../src/assets/HiveMind.png",
+    imgUrl: "./public/assets/HiveMind.png",
     stack: ["NextJS", "Firebase", "Tailwind"],
     link: "https://hive-mind-dun.vercel.app/",
   },
   {
     title: "GitHub Finder",
-    imgUrl: "../src/assets/GitHubFinder.png",
+    imgUrl: "./public/assets/GitHubFinder.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://chirag4862.github.io/GitHub_Finder/",
   },
