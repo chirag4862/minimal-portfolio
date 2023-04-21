f2c61aee9bde6fe3f85ca4cfaa3564b13b89266c
chirag4862 <chiragrvijay@gmail.com>
2023-04-21 23:12:52 +0530

    image path updated

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: "Movie DB",
-    imgUrl: "./public/assets/MovieDB.png",
+    imgUrl: "/MovieDB.png",
     stack: ["SvelteKit", "HTML", "CSS", "JavaScript"],
     link: "https://movie-db-zeta-two.vercel.app/",
   },
   {
     title: "Hive Mind",
-    imgUrl: "./public/assets/HiveMind.png",
+    imgUrl: "/HiveMind.png",
     stack: ["NextJS", "Firebase", "Tailwind"],
     link: "https://hive-mind-dun.vercel.app/",
   },
   {
     title: "GitHub Finder",
-    imgUrl: "./public/assets/GitHubFinder.png",
+    imgUrl: "/GithubFinder.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://chirag4862.github.io/GitHub_Finder/",
   },
